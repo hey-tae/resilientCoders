f6823a398380cb5c25aab63204c86cfc4a7ad007
Tae Wilson <taewilson78@gmail.com>
2021-08-30 14:59:10 -0400

    adding intro js

---
+++ week4/Horoscope/script.js
@@ -0,0 +1,8 @@
+// target inputs
+let birthMonth = document.getElementById('birthMonth')
+let birthDay = document.getElementById('birthDay')
+console.log(birthDay);
+let results = document.getElementById('zodiacReturn')
+
+// console.log(results) --> testing dom selections
+
