f6925c9a69399d697f83903ccce54bdd19fe9f38
Tae Wilson <taewilson78@gmail.com>
2021-08-31 00:50:43 -0400

    added 1 zodiac to horoscope js

--- week4/Horoscope/script.js
+++ week4/Horoscope/script.js
@@ -1,8 +1,28 @@
+function horoscope(){
+
 // target inputs
-let birthMonth = document.getElementById('birthMonth')
-let birthDay = document.getElementById('birthDay')
-console.log(birthDay);
+let birthMonth = document.getElementById('birthMonth').value
+let birthDay = document.getElementById('birthDay').value
+// console.log(birthDay);
 let results = document.getElementById('zodiacReturn')
-
+         document.getElementById('birthMonth').value = '';   
+         document.getElementById('birthDay').value = '';
 // console.log(results) --> testing dom selections
+if (birthMonth == 12 && birthDay > 22 && birthDay <= 31 || birthMonth == 01 && birthDay >= 0 && birthDay <=19){
+             results.innerText = "Capricorn" 
+             console.log(birthMonth)
+             console.log(results)
+             //Capricorn
+         }else if (birthMonth == 01 && birthDay > 20 && birthDay <= 31 || birthMonth == 02 && birthDay > 0 && birthDay <= 18){
+            results.innerText = "Aquarius" 
+            results.appendChild(image)
+            console.log(birthMonth)
+            console.log(results)
+            //Aquarius
+         }else{
+            results.innerText = "Drop and give me 10"
+         }
+         
 
+}
+document.getElementById('submit').addEventListener("click", horoscope);
