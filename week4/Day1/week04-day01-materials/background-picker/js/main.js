d1855664ffab4e4ec8182aab02b21859584f198a
Tae Wilson <taewilson78@gmail.com>
2021-08-31 20:17:34 -0400

    day2 addition

--- week4/Day1/week04-day01-materials/background-picker/js/main.js
+++ week4/Day1/week04-day01-materials/background-picker/js/main.js
@@ -1,7 +1,18 @@
 document.getElementById('purple').onclick = partyPurple
-
+document.getElementById('green').onclick = partyGreen
+document.getElementById('blue').onclick = partyBlue
 
 function partyPurple() {
   document.querySelector('body').style.backgroundColor = 'rgba(241,63,247,1)'
+  document.querySelector('body').style.color = 'white'
+}
+
+function partyGreen(){
+  document.querySelector('body').style.backgroundColor = 'green'
+  document.querySelector('body').style.color = 'white'
+}
+
+function partyBlue(){
+  document.querySelector('body').style.backgroundColor = 'blue'
   document.querySelector('body').style.color = 'white'
 }
