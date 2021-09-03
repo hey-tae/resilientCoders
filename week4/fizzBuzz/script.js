6d0c5e66708a920e9b0db23638ffa621b73b358d
Tae Wilson <taewilson78@gmail.com>
2021-09-03 00:30:01 -0400

    added JWLI Footer

--- week4/fizzBuzz/script.js
+++ week4/fizzBuzz/script.js
@@ -1,9 +1,9 @@
 for(let i=0; i<100; i++){
-    if(parseInt(i % 15) == 0){
+    if((i % 15) == 0){
         console.log('FizzBuzz')
-    }else if(parseInt(i % 3)== 0){
+    }else if((i % 3)== 0){
         console.log('Fizz')
-    }else if (parseInt(i % 5) == 0){
+    }else if ((i % 5) == 0){
         console.log('Buzz')
      }
      else {
